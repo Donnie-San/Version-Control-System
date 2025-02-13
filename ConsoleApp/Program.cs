960e9032e2eddb465cc676520b7fb0ee282d19ea
Donnie Boi <donniesan76@gmail.com>
2025-02-14 05:42:46 +0700

    version 1

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// Today, I will learn git & github, so let's see how it goes.
+
+using System.Globalization;
+
+Console.WriteLine("This is Version 1");
+
+int[] nums = {1, 2, 3, 4, 5};
+foreach (int num in nums) {
+    Console.WriteLine(num);
+}
