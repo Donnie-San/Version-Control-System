1aaebdf4a30688ce1a0733f8a6f0cc7d47185430
Donnie Boi <donniesan76@gmail.com>
2025-02-14 05:59:59 +0700

    version 2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,9 +2,26 @@
 
 using System.Globalization;
 
-Console.WriteLine("This is Version 1");
+Console.WriteLine("Version 2");
+void printArray<T> (T[] array) {
+    foreach (T element in array) {
+        Console.WriteLine(element);
+    }
+}
 
 int[] nums = {1, 2, 3, 4, 5};
+int[] evenNums = new int[5]; int evenCount = 0;
+int[] oddNums = new int[5]; int oddCount = 0;
 foreach (int num in nums) {
-    Console.WriteLine(num);
+    if (num % 2 == 0) {
+        evenNums[evenCount] = num; evenCount++;
+    }
+    else {
+        oddNums[oddCount] = num; oddCount++;
+    }
 }
+
+Console.WriteLine("Even numbers:");
+printArray(evenNums);
+Console.WriteLine("Odd numbers:");
+printArray(oddNums);
